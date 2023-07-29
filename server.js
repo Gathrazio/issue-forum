344d77540d07e6f7795ac10b711283373cfc7c0b
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-29 17:33:49 -0600

    added paths to dist, not build

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'))
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('connected to rtvdb'))
 
-app.use(express.static(path.join(__dirname, 'client', 'dist', 'assets')))
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
 app.use('/api/auth', require('./routes/authRouter.js'))
 app.use('/api/protected', jwt({ secret: process.env.USER_SECRET, algorithms: ['HS256'] }))
 app.use('/api/protected/issues', require('./routes/issueRouter.js'))
