1468a73e0bb41ed9e901b8d0db745403cfacc6f5
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-29 16:57:00 -0600

    changed directory for html file

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
 })
 
 app.listen(9000)
