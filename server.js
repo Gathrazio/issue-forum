2a7d4d90a08f19b84268160319c5fed2091558f7
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-29 16:42:29 -0600

    server preparations for deployment, added favicon

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const { expressjwt: jwt } = require('express-jwt');
+const path = require('path');
 require('dotenv').config()
 
 app.use(express.json())
@@ -11,6 +12,7 @@ app.use(morgan('dev'))
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('connected to rtvdb'))
 
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use('/api/auth', require('./routes/authRouter.js'))
 app.use('/api/protected', jwt({ secret: process.env.USER_SECRET, algorithms: ['HS256'] }))
 app.use('/api/protected/issues', require('./routes/issueRouter.js'))
@@ -22,6 +24,10 @@ app.use((err, req, res, next) => {
         res.status(err.status)
     }
     return res.send({errMsg: err.message})
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 
 app.listen(9000)
